a9f3c64ea5010cbfbbed2644ab042a93cc1b2cc2
anuragcms7654 <anurag_d@cms.co.in>
2026-02-09 21:39:24 +0530

    c

--- experiment.js
+++ experiment.js
@@ -1,22 +1,3 @@
-const arr = [7,6,5,4,9,1,3,4,0]
-const target = 10;
-const output = [];
-const obj = {}
-
-for (let i = 0; i < arr.length; i++){
-    let remaining = target - arr[i];
-    if (remaining in obj){
-        output.push([obj[remaining], i])
-    }
-    obj[arr[i]] = i;
-}
-
-console.log(output);
-
-
-
-
-
-
+const arr = [1,1,1,2,3,4,4,4,5,5,6,6,7,7,8,8,9,9,9,9,9,10];
 
 
